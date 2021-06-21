f2715ab0085da1505179db8f9f1a458a306aa4a8
Steven Middaugh <smiddaugh47@gmail.com>
2021-06-21 11:16:41 -0700

    Express Generator REST API

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const campsiteRouter = express.Router();
+
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send campsite ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.end('Post operation not supported on /campsites/:campsiteId');
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`Will update campsite ${req.params.campsiteId}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting all campsite ${req.params.campsiteId}`);
+});
+
+module.exports = campsiteRouter;
