6cac31e82f9e2f096eabcf77071c0de46980392d
Steven Middaugh <smiddaugh47@gmail.com>
2021-07-09 13:06:53 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '407122950627524',
+        clientSecret: '3e04420b60926abb0c69273e7a2006ce'
+    }
 }
